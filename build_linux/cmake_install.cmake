2dc4ae4897ba13d23275dc693165be8c68b5d006
yurekkk <yurrrashka@gmail.com>
2025-12-03 04:59:19 +1000

    build_linux

--- build_linux/cmake_install.cmake
+++ build_linux/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/knight5016/OS/OS-Lab-1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/knight5016/OS/OS-Lab-1/build_linux/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
